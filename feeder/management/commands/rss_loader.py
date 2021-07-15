ee8ce82285e5055697306d9f01d0f456891cd67c
Michael Savin <jtprogru@gmail.com>
2021-07-15 16:14:28 +0300

    Add async-await
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

--- feeder/management/commands/rss_loader.py
+++ feeder/management/commands/rss_loader.py
@@ -54,7 +54,7 @@ class RSSParser:
         self.source.save()
         logger.info(f'Завершили задание {self.source.title}')
 
-    def get_rss_item_list(self, feed: FeedParserDict) -> List[RSSItem]:
+    async def get_rss_item_list(self, feed: FeedParserDict) -> List[RSSItem]:
         rss_item = []
         for item in feed['entries']:
             try:
@@ -75,14 +75,14 @@ class RSSParser:
 
         return rss_item
 
-    def parse_all(self):
+    async def parse_all(self):
         # Выбрать какое-нибудь задание
         self.find_source()
 
         feed = self.get_rss_feed(self.source.url)
         logger.info(f'Всего постов в RSS-ленте: {len(feed["entries"])}')
 
-        self.get_rss_item_list(feed)
+        await self.get_rss_item_list(feed)
 
         # Завершить задание
         self.finish_source()
@@ -111,4 +111,4 @@ class Command(BaseCommand):
             source.refresh_from_db()
             logger.info('[*] Source {} status -> {}'.format(source.id, source.status))
 
-        rssparser.parse_all()
+        await rssparser.parse_all()
