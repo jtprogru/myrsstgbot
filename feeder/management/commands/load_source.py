30208f832b548ed7459e7f0cf5e71c496c8cf22e
Michael Savin <jtprogru@gmail.com>
2021-07-17 00:58:06 +0300

    Причесал команду load_sources
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

--- feeder/management/commands/load_source.py
+++ feeder/management/commands/load_source.py
@@ -1,15 +1,9 @@
-from datetime import datetime
 from django.conf import settings
-from django.utils import timezone
 from logging import getLogger
 import os
-from pprint import pprint
-from typing import List
 
-from django.core.management import BaseCommand, CommandError
-from django_celery_beat.models import PeriodicTask, IntervalSchedule
+from django.core.management import BaseCommand
 
-from feeder.constants import STATUS_NEW
 from feeder.models import Source
 
 from ruamel.yaml import YAML
@@ -33,17 +27,19 @@ def log_errors(f):
 
 
 @log_errors
-def datetime_parse(date_string: str) -> datetime:
-    """Универсальный парсер даты и времени"""
-    return dateparser.parse(date_string)
-
-
-@log_errors
 def yaml_loader(file_path) -> dict:
     """Загружаем данные из YAML"""
     yaml = YAML()
     with open(file_path, 'r') as f:
         return yaml.load(f.read())
+
+
+def check_source(src_file: str):
+    """Проверяем существование файла"""
+    if src_file:
+        if os.path.exists(os.path.join(settings.BASE_DIR, src_file)):
+            return os.path.join(settings.BASE_DIR, src_file)
+    return None
 
 
 class Loader:
@@ -53,23 +49,15 @@ class Loader:
     def __init__(self, src_file: str = 'sources.yaml'):
         self.source_file = src_file
 
-    def check_source(self, src_file: str):
-        """Проверяем существование файла"""
-        if src_file:
-            if os.path.exists(os.path.join(settings.BASE_DIR, src_file)):
-                return os.path.join(settings.BASE_DIR, src_file)
-        return None
-
     @log_errors
     def load(self):
         """Загрузка данных в БД"""
-        yaml_data = yaml_loader(self.check_source(self.source_file))
+        yaml_data = yaml_loader(check_source(self.source_file))
 
         if "feeds" not in yaml_data:
             msg = f'В файле не объявлен ключ "feeds"'
             logger.info(msg)
             return msg
-
 
         logger.info(f"Источников в файле: {len(yaml_data['feeds'])}")
 
@@ -80,12 +68,11 @@ class Loader:
                     url=source['url'],
                 )
             except Exception as e:
-                logger.info(e)
+                logger.error(e)
 
         logger.info(f'Всего RSS-лент добавлено или обновлено записей: {len(yaml_data["feeds"])}')
 
         return f'Всего RSS-лент добавлено или обновлено записей: {len(yaml_data["feeds"])}'
-
 
 
 class Command(BaseCommand):
